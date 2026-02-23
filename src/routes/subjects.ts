6564fbadca93aaa4f90d2e5efe163900442311e8
markoGodic7 <55702973+markoGodic7@users.noreply.github.com>
2026-02-23 11:40:42 +0100

    changes made inside subjects.ts file

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import {and, desc, eq, getTableColumns, ilike, or, sql} from "drizzle-orm";
-import {departments, subjects} from "../db/Schema";
-import { db } from "../db"
+import {departments, subjects} from "../db/Schema/index.js";
+import { db } from "../db/index.js"
 
 const router = express.Router();
 
