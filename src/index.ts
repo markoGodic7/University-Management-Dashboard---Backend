7cda706205df435c31d135089456599454da2023
markoGodic7 <55702973+markoGodic7@users.noreply.github.com>
2026-02-23 14:54:14 +0100

    implement fetch teachers and create classes -- backend

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import cors from "cors";
 import securityMiddleware from "./middleware/security.js";
 import {toNodeHandler} from "better-auth/node";
 import {auth} from "./lib/auth.js";
+import usersRouter from "./routes/users.js";
+import classesRouter from "./routes/classes.js";
 
 const app = express();
 const PORT = 8000;
@@ -24,6 +26,8 @@ app.use(securityMiddleware);
 app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use('/api/subjects', subjectsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/classes', classesRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the Classroom API!');
