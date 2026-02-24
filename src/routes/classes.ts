50fcdbea78d82d2e68c99bca0d11f8970325413f
markoGodic7 <55702973+markoGodic7@users.noreply.github.com>
2026-02-24 23:57:20 +0100

    implementing changes suggested by coderabbit part 2

--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -92,36 +92,6 @@ router.get("/", async (req, res) => {
 
 // Get classes details with techer, subject and department
 router.get("/:id", async(req, res) => {
-    // const classId = Number(req.params.id);
-    //
-    // if(!Number.isFinite(classId)) return res.status(400).json({ error: "No Class found" });
-    //
-    // const[classDetails] = await db
-    //     .select({
-    //         ...getTableColumns(classes),
-    //         subjects: {
-    //             ...getTableColumns(subjects),
-    //         },
-    //         department: {
-    //             ...getTableColumns(departments),
-    //         },
-    //         teacher: {
-    //             ...getTableColumns(user),
-    //             id: user.id,
-    //             name: user.name,
-    //             image: user.image,
-    //         }
-    //     })
-    //     .from(classes)
-    //     .leftJoin(subjects, eq(classes.subjectId, subjects.id))
-    //     .leftJoin(user, eq(classes.teacherId, user.id))
-    //     .leftJoin(departments, eq(subjects.departmentId, departments.id))
-    //     .where(eq(classes.id, classId))
-    //
-    //     if(!classDetails) return res.status(404).json({ error: "No class found" });
-    //
-    //     res.status(200).json({ data: classDetails });
-
     try {
         const classId = Number(req.params.id);
         if (!Number.isInteger(classId) || classId <= 0) {
@@ -138,7 +108,6 @@ router.get("/:id", async(req, res) => {
                     ...getTableColumns(departments),
                 },
                 teacher: {
-                    ...getTableColumns(user),
                     id: user.id,
                     name: user.name,
                     image: user.image,
