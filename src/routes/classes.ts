f597b9eaeb00d9ed86e77cb8bcff7c09e1a9812c
markoGodic7 <55702973+markoGodic7@users.noreply.github.com>
2026-02-24 23:27:25 +0100

    implement of the class details

--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {classes, subjects, user} from "../db/Schema/index.js";
+import {classes, departments, subjects, user} from "../db/Schema/index.js";
 import {db} from "../db/index.js";
 import {and, desc, eq, getTableColumns, ilike, or, sql} from "drizzle-orm";
 
@@ -89,6 +89,37 @@ router.get("/", async (req, res) => {
         res.status(500).json({ error: "Failed to fetch classes" });
     }
 });
+
+// Get classes details with techer, subject and department
+router.get("/:id", async(req, res) => {
+    const classId = Number(req.params.id);
+
+    if(!Number.isFinite(classId)) return res.status(400).json({ error: "No Class found" });
+
+    const[classDetails] = await db
+        .select({
+            ...getTableColumns(classes),
+            subjects: {
+                ...getTableColumns(subjects),
+            },
+            department: {
+                ...getTableColumns(departments),
+            },
+            teacher: {
+                ...getTableColumns(user)
+            }
+        })
+        .from(classes)
+        .leftJoin(subjects, eq(classes.subjectId, subjects.id))
+        .leftJoin(user, eq(classes.teacherId, user.id))
+        .leftJoin(departments, eq(subjects.departmentId, departments.id))
+        .where(eq(classes.id, classId))
+
+        if(!classDetails) return res.status(404).json({ error: "No class found" });
+
+        res.status(200).json({ data: classDetails });
+
+})
 
 router.post('/', async (req, res) => {
     try {
