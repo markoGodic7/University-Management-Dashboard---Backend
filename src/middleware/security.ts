ec297a90ab63a3a409e77b9a8d34fc4d2a21f2ca
markoGodic7 <55702973+markoGodic7@users.noreply.github.com>
2026-02-25 15:35:24 +0100

    Change in limit of the requests

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -22,7 +22,7 @@ const securityMiddleware = async (req: Request, res: Response, next:NextFunction
                 message='User request limit exceeded (10 per minute). Please wait.';
                 break;
             default:
-                limit=20;
+                limit=5;
                 message='Guest request limit exceeded (5 per minute). Please sign up for higher limits.';
                 break;
         }
