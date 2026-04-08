99a5da3dc40f6bcd20eb3c4e0772e9eba3fa5eb6
markoGodic7 <markenstein97@gmail.com>
2026-04-08 12:46:55 +0200

    changes in the lim

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -13,17 +13,17 @@ const securityMiddleware = async (req: Request, res: Response, next:NextFunction
 
         switch (role) {
             case 'admin':
-                limit=120;
+                limit=20;
                 message='AdminS request limit exceeded (20 per minute). Slow down.';
                 break;
             case 'teacher':
             case 'student':
-                limit=50;
+                limit=10;
                 message='User request limit exceeded (10 per minute). Please wait.';
                 break;
             default:
-                limit=50;
-                message='Guest request limit exceeded (50 per minute). Please sign up for higher limits.';
+                limit=5;
+                message='Guest request limit exceeded (5 per minute). Please sign up for higher limits.';
                 break;
         }
 
